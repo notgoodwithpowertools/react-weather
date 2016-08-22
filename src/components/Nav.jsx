b6f3394b5cbf97c44ee628fe539cb57bc1526936
Andrew <notgoodwithpowertools@gmail.com>
2016-08-22 20:17:05 +1000

    Styled Nav Bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,63 @@
 var React = require('react');
 
 var {Link, IndexLink} = require('react-router');
+
+//Refactor for Foundation
+var Nav = React.createClass({
+
+  onSearch: function(e){
+    e.preventDefault();
+    alert('Not wired up yet');
+  },
+
+  render: function(){
+    return (
+
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">
+              React Weather App
+            </li>
+            <li>
+              <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Weather</IndexLink>
+            </li>
+            <li>
+              <Link to="/about" activeClassName="active" activeStyle={{fontWeight:'bold'}}>About</Link>
+            </li>
+            <li>
+              <Link to="/examples" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Examples</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <form onSubmit={this.onSearch}>
+            <ul className="menu">
+              <li>
+                <input type="search" placeholder="Search Weather"/>
+              </li>
+              <li>
+                <input type="submit" className="button" value="Get Weather "/>
+
+              </li>
+            </ul>
+
+          </form>
+        </div>
+
+      </div>
+
+    );
+
+  }
+
+});
+
+
+
+module.exports = Nav;
+
+//Build component using React.createClass
 /*
 var Nav = React.createClass({
   render: function(){
@@ -16,6 +73,7 @@ var Nav = React.createClass({
 });
 */
 //Refactor as a stateless functional component
+/*
 var Nav = () => {
     return(
     <div>
@@ -26,5 +84,4 @@ var Nav = () => {
     </div>
   )
 };
-
-module.exports = Nav;
+*/
